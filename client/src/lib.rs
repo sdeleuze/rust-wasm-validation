7d4505c9ce68d74f31767ecc6cedfe663c445e62
Sébastien Deleuze <sdeleuze@vmware.com>
2022-04-12 16:35:13 +0200

    Add a basic client implementation

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,3 +1,12 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(start)]
+pub fn run() {
+    let window = web_sys::window().expect("no global `window` exists");
+    window.alert_with_message("Hello Rust").unwrap();
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
