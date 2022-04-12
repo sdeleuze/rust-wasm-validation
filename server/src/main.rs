a80aca5e042efe363d71ee259ff1ac7257939d42
Sébastien Deleuze <sdeleuze@vmware.com>
2022-04-12 16:51:54 +0200

    Add a basic Axum server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{response::Html, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(handler));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    println!("listening on {addr}");
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>hello, world!</h1>")
 }
